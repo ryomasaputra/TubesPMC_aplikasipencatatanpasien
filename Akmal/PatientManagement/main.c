d652277584774ba200d64726c1f60677f8efcccf
akmal hafizh musyaffa <118372685+akmalhfzh@users.noreply.github.com>
2024-06-11 01:17:47 +0700

    Add files via upload

--- Akmal/PatientManagement/main.c
+++ Akmal/PatientManagement/main.c
@@ -1,68 +1,33 @@
-#include <gtk/gtk.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "patient.h"
+// #include <gtk/gtk.h>
+#include <C:/msys64/mingw64/include/gtk-3.0/gtk/gtk.h>
 
-// Function to add patient to database
-void add_patient_to_database() {
-    // Implement logic to add patient to database
-    printf("Adding patient to database...\n");
-    return;
+static void print_hello(GtkWidget *widget, gpointer data) { g_print("Hello World\n"); }
+
+static void activate(GtkApplication *app, gpointer user_data) {
+  GtkWidget *window;
+  GtkWidget *label = gtk_label_new (NULL);
+
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "GTK-4.0 Test Window");
+  gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+  gtk_label_set_markup (GTK_LABEL (label), "Hello World");
+  gtk_label_set_xalign(GTK_LABEL (label), 0.5f);
+  gtk_label_set_yalign(GTK_LABEL (label), 0.5f);
+
+  gtk_window_set_child(GTK_WINDOW(window), label);
+  gtk_window_present(GTK_WINDOW(window));
 }
 
-// Function to display patient information from database
-void display_patient_info_from_database() {
-    // Implement logic to display patient information from database
-    printf("Displaying patient information...\n");
-    return;
+int main(int argc, char **argv) {
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
 }
-
-// Callback function for adding patient
-void add_patient(GtkWidget *widget, gpointer data) {
-    add_patient_to_database();
-    GtkWidget *message = gtk_message_dialog_new(GTK_WINDOW(widget), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "Patient added successfully");
-    gtk_dialog_run(GTK_DIALOG(message));
-    gtk_widget_destroy(message);
-}
-
-// Callback function for displaying patient information
-void display_patient_info(GtkWidget *widget, gpointer data) {
-    display_patient_info_from_database();
-    GtkWidget *message = gtk_message_dialog_new(GTK_WINDOW(widget), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "Patient information displayed successfully");
-    gtk_dialog_run(GTK_DIALOG(message));
-    gtk_widget_destroy(message);
-}
-
-int main(int argc, char *argv[]) {
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *add_button;
-    GtkWidget *display_button;
-
-    gtk_init(&argc, &argv);
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Patient Management System");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
-
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    add_button = gtk_button_new_with_label("Add Patient");
-    g_signal_connect(add_button, "clicked", G_CALLBACK(add_patient), NULL);
-    gtk_grid_attach(GTK_GRID(grid), add_button, 0, 0, 1, 1);
-
-    display_button = gtk_button_new_with_label("Display Patient Info");
-    g_signal_connect(display_button, "clicked", G_CALLBACK(display_patient_info), NULL);
-    gtk_grid_attach(GTK_GRID(grid), display_button, 1, 0, 1, 1);
-
-    gtk_widget_show_all(window);
-
-    gtk_main();
-
-    return EXIT_SUCCESS;
-}
-
